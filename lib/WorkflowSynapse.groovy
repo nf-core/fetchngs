c2029b3f8a704175a0d4b1ef4b41fcf43bd483cf
Daisy Wenyan Han <60151111+daisyhan97@users.noreply.github.com>
2021-10-15 12:48:24 -0700

    Add RegEx for SampleIDs

--- lib/WorkflowSynapse.groovy
+++ lib/WorkflowSynapse.groovy
@@ -35,4 +35,57 @@ class WorkflowSynapse {
             "  in a standardised manner when uploading data to Synapse.\n" +
             "==================================================================================="
     }
+
+    //
+    // Obtain Sample ID from File Name
+    //
+    public static String getSampleID(input_file, pattern) {
+
+        def sampleids = ""
+
+        def filePattern = pattern.toString()
+        int p = filePattern.lastIndexOf('/')
+            if( p != -1 )
+                filePattern = filePattern.substring(p+1)
+
+        input_file.each {
+            String fileName = input_file.getFileName().toString()
+
+            String indexOfWildcards = filePattern.findIndexOf { it=='*' || it=='?' }
+            String indexOfBrackets = filePattern.findIndexOf { it=='{' || it=='[' }
+            if( indexOfWildcards==-1 && indexOfBrackets==-1 ) {
+                if( fileName == filePattern )
+                    return actual.getSimpleName()
+                throw new IllegalArgumentException("Not a valid file pair globbing pattern: pattern=$filePattern file=$fileName")
+            }
+
+            int groupCount = 0
+            for( int i=0; i<filePattern.size(); i++ ) {
+                def ch = filePattern[i]
+                if( ch=='?' || ch=='*' )
+                    groupCount++
+                else if( ch=='{' || ch=='[' )
+                    break
+            }
+
+            def regex = filePattern
+                    .replace('.','\\.')
+                    .replace('*','(.*)')
+                    .replace('?','(.?)')
+                    .replace('{','(?:')
+                    .replace('}',')')
+                    .replace(',','|')
+
+            def matcher = (fileName =~ /$regex/)
+            if( matcher.matches() ) {
+                int c=Math.min(groupCount, matcher.groupCount())
+                int end = c ? matcher.end(c) : ( indexOfBrackets != -1 ? indexOfBrackets : fileName.size() )
+                def prefix = fileName.substring(0,end)
+                while(prefix.endsWith('-') || prefix.endsWith('_') || prefix.endsWith('.') )
+                    prefix=prefix[0..-2]
+                sampleids = prefix
+            }
+        }
+        return sampleids
+    }
 }
