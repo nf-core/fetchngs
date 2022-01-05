b9d5caf9f07bab8b6e05759b37449ddc452a8116
kaitlinchaung <kaitlinchaung@gmail.com>
2022-01-04 18:55:27 -0800

    syntax

--- bin/dgmfinder.py
+++ bin/dgmfinder.py
@@ -46,8 +46,9 @@ def main():
 
     with gzip.open(args.fastq_file, 'rt') as handle:
         head = [next(handle) for x in range(2)]
-        read_len = len(head[1].strip())
-        distance = (read_len - 2 * args.kmer_size) / 2
+
+    read_len = len(head[1].strip())
+    distance = (read_len - 2 * args.kmer_size) / 2
 
     logging.info(f'============INPUTS============')
     logging.info(f'fastq_id         : {args.fastq_id}')
